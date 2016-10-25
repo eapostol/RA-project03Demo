6676c61a30ba2b6f20bb9efd2944b9312a1fe912
Edward Apostol <edward.apostol.github@gmail.com>
2016-10-25 16:40:20 -0400

    #013: added basic Best Buy API object
    added basic Best Buy API Object

---
+++ src/model/BBAPIObject.js
@@ -0,0 +1,16 @@
+export default class BBAPIObject{
+
+    constructor(data){
+        this.from = data['from'];
+        this.to = data['to'];
+        this.total = data['total'];
+        this.currentPage = data['currentPage'];
+        this.totalPages =  data['totalPages'];
+        this.queryTime = data['queryTime'];
+        this.totalTime = data['totalTime'];
+        this.partial = data['partial'];
+        this.canonicalUrl = data['canonicalUrl'];
+        this.products = data['products']
+    }
+
+}
